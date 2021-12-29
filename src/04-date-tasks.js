fafbfc61fda6a2c5bbaf43cfb2e88472206379e4
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-12-29 19:38:58 +0300

    feat: implement angleBetweenClockHands function

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -103,8 +103,12 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const hours = date.getUTCHours() % 12;
+  const minutes = date.getMinutes();
+  const angle = Math.abs(0.5 * (60 * hours - 11 * minutes));
+  const normalizedAngle = angle > 180 ? angle - 180 : angle;
+  return normalizedAngle * (Math.PI / 180);
 }
 
 
