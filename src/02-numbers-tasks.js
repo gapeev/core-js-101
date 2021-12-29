c621d6d3eb9ef829d81ce7fea86e54da943b95a1
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-12-29 11:37:16 +0300

    feat: implement getAverage function

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -51,8 +51,8 @@ function getCircleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return Math.trunc(value1 / 2) + Math.trunc(value2 / 2) + (value1 % 2) / 2 + (value2 % 2) / 2;
 }
 
 /**
