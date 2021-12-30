0f7ce3e8bf4f62edadf241541bc890f8cb1ad662
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-12-30 16:49:36 +0300

    feat: implement getDigitalRoot function

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -302,8 +302,12 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num < 10) {
+    return num;
+  }
+  const numbers = String(num).split('').map(Number);
+  return getDigitalRoot(numbers.reduce((acc, number) => acc + number, 0));
 }
 
 
