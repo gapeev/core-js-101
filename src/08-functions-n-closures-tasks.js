274c2ef35b31f09a0a5a3a5cb7244db248a6e7a0
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-12-31 10:24:53 +0300

    feat: implement retry function

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -106,8 +106,17 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = attempts; i > 0;) {
+      try {
+        return func();
+      } catch (error) {
+        i -= 1;
+      }
+    }
+    return undefined;
+  };
 }
 
 
