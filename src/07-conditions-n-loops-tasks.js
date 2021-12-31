349ad9f12e4d0275966219710137a4c9dc613a70
Dmitry Gapeev <gapeev.dm@gmail.com>
2021-12-31 09:47:53 +0300

    feat: implement evaluateTicTacToePosition function

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -467,8 +467,18 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
+function evaluateTicTacToePosition(position) {
+  const isWinner = (a, b, c) => a === b && a === c && a !== undefined;
+
+  for (let i = 0; i < 3; i += 1) {
+    if (isWinner(position[i][0], position[i][1], position[i][2])) return position[i][0];
+    if (isWinner(position[0][i], position[1][i], position[2][i])) return position[0][i];
+  }
+
+  if (isWinner(position[0][0], position[1][1], position[2][2])) return position[1][1];
+  if (isWinner(position[2][0], position[1][1], position[0][2])) return position[1][1];
+
+  return undefined;
 }
 
 
